aee000b4ee627c3a2da3d5445dcf1b248b9c0bed
GS00078 <148823987+GS00078@users.noreply.github.com>
2025-04-02 13:31:56 -0400

    Update TestIdentifySpam.java

--- code/JUnit Sample/src/test/java/edu/westga/comp4420/junit_sample/test/model/telephone_checker/TestIdentifySpam.java
+++ code/JUnit Sample/src/test/java/edu/westga/comp4420/junit_sample/test/model/telephone_checker/TestIdentifySpam.java
@@ -9,5 +9,57 @@ import org.junit.jupiter.api.Test;
 import edu.westga.comp4420.junit_sample.model.TelephoneChecker;
 
 class TestIdentifySpam {
-
+	
+	@Test
+	void testValidNumber() {
+		
+	TelephoneChecker checker = new TelephoneChecker();
+		assertFalse(checker.identifySpam(1111234567L));
+		
+	}
+	
+	@Test
+	void testInvalidNumber() {
+	TelephoneChecker checker = new TelephoneChecker();
+		assertThrows(IllegalArgumentException.class, () -> {
+			checker.identifySpam(555555L);
+		});
+		
+	}
+	
+	@Test
+	void testSpamAreaCode999() {
+		TelephoneChecker checker = new TelephoneChecker();
+		assertTrue(checker.identifySpam(9995555555L));
+	}
+	
+	@Test
+	void testSpamAreaCode900() {
+		TelephoneChecker checker = new TelephoneChecker();
+		assertTrue(checker.identifySpam(9005555555L));
+	}
+	
+	@Test
+	void testSpamAreaCode888() {
+		TelephoneChecker checker = new TelephoneChecker();
+		assertTrue(checker.identifySpam(8885555555L));
+	}
+	
+	@Test
+	void testSpamAreaCode800() {
+		TelephoneChecker checker = new TelephoneChecker();
+		assertTrue(checker.identifySpam(8005555555L));
+	}
+	
+	@Test
+	void testSpamAreaCode911() {
+		TelephoneChecker checker = new TelephoneChecker();
+		assertTrue(checker.identifySpam(9115555555L));
+	}
+	
+	@Test
+	void testSpamAreaCode411() {
+		TelephoneChecker checker = new TelephoneChecker();
+		assertTrue(checker.identifySpam(4115555555L));
+	}
 }
